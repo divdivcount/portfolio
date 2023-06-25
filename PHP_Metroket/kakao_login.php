6c8826b8b73ebd1472cb52bb968a1b318b26f0b6
장권용 <49908463+divdivcount@users.noreply.github.com>
2023-06-25 13:59:21 +0900

    Update kakao_login.php

--- PHP_Metroket/kakao_login.php
+++ PHP_Metroket/kakao_login.php
@@ -1,5 +1,5 @@
 <?php
- $restAPIKey = "24c58b732b0414e7d5d850cdafc310b8"; //본인의 REST API KEY를 입력해주세요
+ $restAPIKey = ""; //본인의 REST API KEY를 입력해주세요
  $callbacURI = urlencode("https://metroket.kro.kr/kakao_callback.php"); //본인의 Call Back URL을 입력해주세요
  $kakaoLoginUrl = "https://kauth.kakao.com/oauth/authorize?client_id=".$restAPIKey."&redirect_uri=".$callbacURI."&response_type=code";
 ?>
